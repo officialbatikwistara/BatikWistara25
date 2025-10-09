92e1a9409e8e17e8eeb00804696ab1afc2ae139b
LovinaPutriPratama <23081010018@student.upnjatim.ac.id>
2025-10-09 10:33:39 +0700

    update

--- kontak.php
+++ kontak.php
@@ -7,5 +7,36 @@
   <p>WhatsApp: <a href="https://wa.me/6281234567890">0812-3456-7890</a></p>
   <p>Email: official.batikwistara@gmail.com</p>
 </section>
+<section class="kontak-container">
+  <h2><i class="fa-solid fa-envelope"></i> Kontak Kami</h2>
+  <div class="kontak-box">
+    <div class="kontak-item">
+      <i class="fa-solid fa-map-marker-alt"></i>
+      <div>
+        <h4>Alamat</h4>
+        <p>Jl. Tambak Medokan Ayu VI C No.56B, Surabaya, Jawa Timur 60295</p>
+      </div>
+    </div>
+
+    <div class="kontak-item">
+      <i class="fa-brands fa-whatsapp"></i>
+      <div>
+        <h4>WhatsApp</h4>
+        <a href="https://wa.me/6281234567890" target="_blank">0812-3456-7890</a>
+      </div>
+    </div>
+
+    <div class="kontak-item">
+      <i class="fa-solid fa-envelope"></i>
+      <div>
+        <h4>Email</h4>
+        <a href="mailto:official.batikwistara@gmail.com">official.batikwistara@gmail.com</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- tambahkan Font Awesome di <head> -->
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
 <?php include 'inc/footer.php'; ?>
